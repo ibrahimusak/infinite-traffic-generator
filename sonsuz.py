5916334b0a2ae0617c577180aeaeb5b475dfa7e7
ibrahimusak <54116015+ibrahimusak@users.noreply.github.com>
2019-08-14 20:17:04 +0300

    Create sonsuz.py

---
+++ sonsuz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from subprocess import Popen
+import os,sys
+import time
+filename = sys.argv[1]
+a = 0
+while True:
+	while a<=20:
+		print("islem yeniden basladi")
+		os.system("killall Xvfb")
+		os.system("killall webkit_server")
+		p = Popen("python " + filename, shell=True)     
+		p.wait()
+		a += 1
+	while a>20:
+		print("proxy toplaniyor")
+		os.system("killall Xvfb")
+		os.system("killall webkit_server")
+		p = Popen("python3 "+"proxy.py",  shell=True)     
+		p.wait()
+		a -=20
